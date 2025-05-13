edbd97edb3dd5799769ca42bdbb3efb6b1375519
Adrian Plapamaru <adrian.plapamaru@yahoo.com>
2025-05-13 09:12:38 +0300

    Draw

---
+++ Assets/Scripts/DrawArea.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawArea : MonoBehaviour {
+
+	[SerializeField] private List<Vector3> points = new();
+
+	private void OnDrawGizmos() {
+		
+	}
+}
--- Assets/Scripts/DrawController.cs
+++ Assets/Scripts/DrawController.cs
@@ -1,16 +1,11 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DrawController : MonoBehaviour {
 
-	
-
 	[SerializeField] private CanvasScaler canvasScaler;
 	[SerializeField] private RawImage rawImage;
 	[SerializeField] private RawImage brushImage;
-	[SerializeField] private Color penColor;
-	[SerializeField] private int brushSize;
 
 	private Texture2D texture;
 	private int textureWidth;
@@ -23,9 +18,9 @@ public class DrawController : MonoBehaviour {
 	private int brushHeight;
 
 	private Vector2Int prevPixel = new(-1, -1);
-	private Vector2 prevBrushAnchorPos;
 
 	private void Start() {
+		Application.targetFrameRate = 60;
 		SetTexture();
 		SetBrushPixels();
 	}
@@ -57,60 +52,22 @@ public class DrawController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.R)) {
 			Start();
 		}
-		
-		if (Input.GetMouseButtonDown(0)) {
-			brushImage.gameObject.SetActive(true);
-			prevPixel = GetTexturePixel();
-			if (RectTransformUtility.ScreenPointToLocalPointInRectangle(surfaceRect, Input.mousePosition, null, out Vector2 localPoint)) {
-				brushImage.rectTransform.anchoredPosition = localPoint;
-				prevBrushAnchorPos = localPoint;
-			}
-		}
+
 		if (Input.GetMouseButton(0)) {
+			brushImage.gameObject.SetActive(true);
 			Vector2Int pixel = GetTexturePixel();
 			if (prevPixel != pixel) {
 				if (RectTransformUtility.ScreenPointToLocalPointInRectangle(surfaceRect, Input.mousePosition, null, out Vector2 localPoint)) {
-					
 					brushImage.rectTransform.anchoredPosition = localPoint;
-
-					// if (Vector2.Distance(localPoint, prevBrushAnchorPos) > 10f) {
-					// 	Vector2 dir = (localPoint - prevBrushAnchorPos).normalized;
-					// 	brushImage.transform.up = dir;
-					// 	prevBrushAnchorPos = localPoint;
-					// }
-					
-					// Vector2 prevLocal = GetLocalPositionFromPixel(prevPixel);
-					// Vector2 dir = (localPoint - prevLocal).normalized;
-					// brushImage.rectTransform.up = dir;
-					// float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-					// brushImage.rectTransform.rotation = Quaternion.Euler(0, 0, angle);
-
-					DrawBrushImage(prevPixel, pixel);
+					PaintStampAt(GetLocalPositionFromPixel(pixel));
+					texture.Apply();
 				}
 				prevPixel = pixel;
 			}
-			
 		} else if (Input.GetMouseButtonUp(0)) {
 			brushImage.gameObject.SetActive(false);
+			prevPixel = new Vector2Int(-1, -1);
 		}
-	}
-
-	private void DrawBrushImage(Vector2Int from, Vector2Int to) {
-		
-		/*int steps = Mathf.CeilToInt(Vector2Int.Distance(from, to));
-		for (int i = 0; i <= steps; i++) {
-			float t = i / (float)steps;
-			Vector2Int pixel = Vector2Int.RoundToInt(Vector2.Lerp(from, to, t));
-			Vector2 localPoint = GetLocalPositionFromPixel(pixel);
-
-			brushImage.rectTransform.anchoredPosition = localPoint;
-
-			prevBrushAnchorPos = localPoint;
-
-			PaintStampAt(localPoint);
-		}*/
-		PaintStampAt(GetLocalPositionFromPixel(to));
-		texture.Apply();
 	}
 	
 	private void PaintStampAt(Vector2 localPoint) {
@@ -146,85 +103,6 @@ public class DrawController : MonoBehaviour {
 			}
 		}
 	}
-	
-	/*private void PaintStampAt(Vector2 localPoint) {
-		float dx = (localPoint.x + surfaceRect.rect.width / 2) / surfaceRect.rect.width;
-		float dy = (localPoint.y + surfaceRect.rect.height / 2) / surfaceRect.rect.height;
-		int baseX = Mathf.RoundToInt(dx * textureWidth) - brushWidth / 2;
-		int baseY = Mathf.RoundToInt(dy * textureHeight) - brushHeight / 2;
-
-		for (int x = 0; x < brushWidth; x++) {
-			for (int y = 0; y < brushHeight; y++) {
-				int dstX = baseX + x;
-				int dstY = baseY + y;
-				
-				Vector2Int dstPixel = new Vector2Int(dstX, dstY);
-				if (paintedPixelsThisStroke.Contains(dstPixel)) {
-					continue;
-				}
-				
-				if (dstX < 0 || dstX >= textureWidth || dstY < 0 || dstY >= textureHeight) {
-					continue;
-				}
-
-				Color brushColor = brushPixels[y * brushWidth + x];
-				if (brushColor.a == 0) {
-					continue;
-				}
-
-				paintedPixelsThisStroke.Add(dstPixel);
-				texture.SetPixel(dstX, dstY, brushColor);
-			}
-		}
-	}*/
-	
-	/*private void DrawBrushImage() {
-		Vector2 anchoredPos = brushImage.rectTransform.anchoredPosition;
-		float dx = (anchoredPos.x + surfaceRect.rect.width / 2) / surfaceRect.rect.width;
-		float dy = (anchoredPos.y + surfaceRect.rect.height / 2) / surfaceRect.rect.height;
-		int baseX = Mathf.RoundToInt(dx * textureWidth) - brushWidth / 2;
-		int baseY = Mathf.RoundToInt(dy * textureHeight) - brushHeight / 2;
-		for (int x = 0; x < brushWidth; x++) {
-			for (int y = 0; y < brushHeight; y++) {
-				int dstX = baseX + x;
-				int dstY = baseY + y;
-
-				if (dstX < 0 || dstX >= textureWidth || dstY < 0 || dstY >= textureHeight) {
-					continue;
-				}
-
-				Color brushColor = brushPixels[y * brushWidth + x];
-				if (brushColor.a == 0) {
-					continue;
-				}
-
-				texture.SetPixel(dstX, dstY, brushColor); // You can blend here if desired
-			}
-		}
-		texture.Apply();
-	}*/
-
-	private void Draw(Vector2Int from, Vector2Int to) {
-		int dx = Mathf.Abs(to.x - from.x);
-		int dy = Mathf.Abs(to.y - from.y);
-		int steps = Mathf.Max(dx, dy);
-		
-		/*for (int i = 0; i <= steps; i++) {
-			float t = i / (float)steps;
-			int x = Mathf.RoundToInt(Mathf.Lerp(from.x, to.x, t));
-			int y = Mathf.RoundToInt(Mathf.Lerp(from.y, to.y, t));
-			// DrawPixel(x, y);
-			// DrawCircle(x, y);
-		}*/
-		
-		texture.Apply();
-	}
-	
-	private void DrawPixel(int x, int y) {
-		if (x >= 0 && x < textureWidth && y >= 0 && y < textureHeight) {
-			texture.SetPixel(x, y, penColor);
-		}
-	}
 
 	private Vector2Int GetTexturePixel() {
 		RectTransformUtility.ScreenPointToLocalPointInRectangle(surfaceRect, Input.mousePosition, null, out Vector2 localPoint);
@@ -238,31 +116,6 @@ public class DrawController : MonoBehaviour {
 		float px = (pixel.x / (float)textureWidth) * surfaceRect.rect.width - surfaceRect.rect.width / 2f;
 		float py = (pixel.y / (float)textureHeight) * surfaceRect.rect.height - surfaceRect.rect.height / 2f;
 		return new Vector2(px, py);
-	}
-
-	private static void Navigate(int from, int to, Action<int> navigate) {
-		if (from < to) {
-			for (int i = from; i <= to; i++) {
-				navigate(i);
-			}
-		} else {
-			for (int i = from; i >= to; i--) {
-				navigate(i);
-			}
-		}
-	}
-
-	private void DrawCircle(int cx, int cy) {
-		for (int x = -brushSize; x <= brushSize; x++) {
-			for (int y = -brushSize; y <= brushSize; y++) {
-				if (x * x + y * y <= brushSize * brushSize) {
-					int px = Mathf.Clamp(cx + x, 0, textureWidth - 1);
-					int py = Mathf.Clamp(cy + y, 0, textureHeight - 1);
-					texture.SetPixel(px, py, penColor);
-				}
-			}
-		}
-		texture.Apply();
 	}
 
 	private static Color[] GetResizedPixels(Texture2D originalTexture, int newWidth, int newHeight) {
